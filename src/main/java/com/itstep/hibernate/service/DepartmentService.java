f1bed7e6560cc13109d2aa30e78eac8a7f552774
andreykonovaluk <andreykonovaluk1990@gmail.com>
2022-12-22 21:11:34 +0300

    Added v1

--- src/main/java/com/itstep/hibernate/service/DepartmentService.java
+++ src/main/java/com/itstep/hibernate/service/DepartmentService.java
@@ -4,13 +4,35 @@ import com.itstep.hibernate.dao.models.Department;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class DepartmentService {
 
-    public List<Department> getAllDepartments () {
+    public List<Department> getAllDepartments() {
+        Transaction transaction = null;
+        List<Department> resultSet = null;
         try (Session session = HibernateConfiguration.getSessionFactory().openSession()) {
-            return session.createQuery("from department", Department.class).list();
+
+            transaction = session.beginTransaction();
+
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Department> cq = cb.createQuery(Department.class);
+            Root<Department> rootEntry = cq.from(Department.class);
+            CriteriaQuery<Department> all = cq.select(rootEntry);
+            TypedQuery<Department> allQuery = session.createQuery(all);
+            resultSet = allQuery.getResultList();
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
+        return resultSet;
     }
 }
